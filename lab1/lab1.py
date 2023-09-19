4a3b3de4c308ed8434315296b5b0e4d8a58f016a
arioo_ <arianfooladray@gmail.com>
2023-09-19 19:54:56 -0400

    organized and fixed b.3

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,63 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#function that will handle plotting
+# Function for plotting
 def plot(f_t, t, newGraph=True, figsize=(8.0, 4.0), title='', plotLabel='', xLabel='', yLabel=''):
     if newGraph:
-        plt.figure(figsize=figsize)  # Create a new figure for each plot if param is untouched
+        plt.figure(figsize=figsize)
     plt.plot(t, f_t, label=plotLabel)
     plt.title(title)
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
-    plt.grid()
+    plt.grid(True)
     plt.legend()
     plt.tight_layout()
 
 # Problem A.1
-t = np.linspace(-2, 2, 8)  # defining 1000 points between -2 and 2
-f_t = np.exp(-t) * np.cos(2 * np.pi * t)  # defining function
+t = np.linspace(-2, 2, 8)
+f_t = np.exp(-t) * np.cos(2 * np.pi * t)
 plot(f_t, t, figsize=(8.0, 4.0), title='f(t) = e^(-t) * cos(2πt) plot', plotLabel='e^(-t) * cos(2πt)', xLabel='t', yLabel='f(t)')
 
-t = np.linspace(-2, 2, 400)  # -2:0.1:2
-f_t = np.exp(-t) * np.cos(2 * np.pi * t)  # defining function
-plot(f_t, t, figsize=(8.0, 4.0), title='f(t) = e^(-t) * cos(2πt) plot', plotLabel='e^(-t) * cos(2πt)', xLabel='t', yLabel='f(t)')
+t = np.linspace(-2, 2, 400)
+f_t = np.exp(-t) * np.cos(2 * np.pi * t)
+plot(f_t, t, newGraph=False, figsize=(8.0, 4.0), title='f(t) = e^(-t) * cos(2πt) plot', plotLabel='e^(-t) * cos(2πt)', xLabel='t', yLabel='f(t)')
 
 # Problem A.2
-t = np.linspace(-2, 2, 5)  # 5 points from -2 to 2
-f_t = np.exp(t) #e^t
+t = np.linspace(-2, 2, 5)
+f_t = np.exp(t)
 plot(f_t, t, figsize=(8.0, 4.0), title='f(t) = e^(-t) plot', plotLabel='e^(-t)', xLabel='t', yLabel='f(t)')
-plt.xticks(np.arange(-2, 2.1, 1))  # adjust the ticks so they also go from -2 to 2 in 5 increments
+plt.xticks(np.arange(-2, 2.1, 1))
 
 # Problem A.3
-t = np.linspace(-2, 2, 8)  # defining 1000 points between -2 and 2
-f_t = np.exp(-t) * np.cos(2 * np.pi * t)  # defining function
-plot(f_t, t,figsize=(8.0, 4.0), title='f(t) = e^(-t) * cos(2πt) plot', plotLabel='e^(-t) * cos(2πt)', xLabel='t', yLabel='f(t)')
-t = np.linspace(-2, 2, 5)  # 5 points from -2 to 2
+t = np.linspace(-2, 2, 8)
+f_t = np.exp(-t) * np.cos(2 * np.pi * t)
+plot(f_t, t, figsize=(8.0, 4.0), title='f(t) = e^(-t) * cos(2πt) plot', plotLabel='e^(-t) * cos(2πt)', xLabel='t', yLabel='f(t)')
+
+t = np.linspace(-2, 2, 5)
 f_t = np.exp(t)
-plt.grid(True) #grid was not showing up until this 
-plot(f_t, t, False, figsize=(8.0, 4.0), title='f(t) = e^(-t) plot', plotLabel='e^(-t)', xLabel='t', yLabel='f(t)')
-plt.grid(True) #grid was not showing up until this 
+plot(f_t, t, newGraph=False, figsize=(8.0, 4.0), title='f(t) = e^(-t) plot', plotLabel='e^(-t)', xLabel='t', yLabel='f(t)')
 
 # Problem B.1
-t = np.linspace(-1,2, 1000) #defining 1000 points between -1 and 2 
-p_t = np.heaviside(t, 1) - np.heaviside(t - 1, 1) #unit step function
+t = np.linspace(-1, 2, 1000)
+p_t = np.heaviside(t, 1) - np.heaviside(t - 1, 1)
 plot(p_t, t, figsize=(8.0, 4.0), title='p(t) = u(t)−u(t −1) over (−1 ≤ t ≤ 2).', plotLabel='u(t)−u(t −1)', xLabel='t', yLabel='p(t)')
 
 # Problem B.2
-    # function for r(t) as its used more than once
 def r(t):
     return t * p_t
 
-r_t = r(t) #defining r_t
-n_t = r(t) + r(-t +2) #defining n_t
+def n(t):
+    return r(t) + r(-t + 2)
 
-    # plotting both on the same graph
-plot(r_t,t,figsize=(8.0, 4.0), title='r(t) = tp(t)', plotLabel='r(t) = tp(t)', xLabel='t',yLabel='r(t)')
-plot(n_t,t, newGraph=False, figsize=(8,4), title='r(t) = tp(t) & n(t) =r(t) + r(−t + 2).', plotLabel='n(t) = r(t) + r(−t + 2).', xLabel='t',yLabel='n(t)')
-plt.grid(True) #plot grid wasnt showing up found this solution
+r_t = r(t)
+n_t = n(t)
+
+plot(r_t, t, figsize=(8.0, 4.0), title='r(t) = tp(t)', plotLabel='r(t) = tp(t)', xLabel='t', yLabel='r(t)')
+plot(n_t, t, newGraph=False, figsize=(8, 4), title='r(t) = tp(t) & n(t) =r(t) + r(−t + 2).', plotLabel='n(t) = r(t) + r(−t + 2).', xLabel='t', yLabel='n(t)')
 
 # Problem B.3
+t = np.linspace(-1, 1, 1000)  # Adjust the time values for n1 and n2
+n1_t = n(0.5 * t)
+plot(n1_t, t, figsize=(8.0, 4.0), title='n1(t) = n(1/2 t)', plotLabel='n1(t) = n(1/2 t)', xLabel='t', yLabel='n1(t)')
+t = t + (1/2)
+n2_t = n(0.5 * (t))  # Adjust the time values for n2
+# Plotting n1 and n2
+plot(n2_t, t, newGraph=False, figsize=(8.0, 4.0), title='n2(t) = n1(t + 1/2)', plotLabel='n2(t) = n1(t + 1/2)', xLabel='t', yLabel='n2(t)')
 
-
-
-plt.show()  # show all the plots (last line)
+plt.show()
